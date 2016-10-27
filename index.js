ee48b94c4084bf4f83626c2619bb1709c237301f
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-10-27 20:44:24 -0300

    se agrega body-parser para capturar parametros

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var bodyParser = require("body-parser")
 
 var app = express();
 
-
 app.use("/app",express.static("public"))
+app.use(bodyParser.json()) // para peticiones application/json
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine","jade");
 
@@ -13,6 +15,13 @@ app.get("/", function (req, res) {
 
 app.get("/login", function (req, res) {
   res.render("login")
+})
+
+app.post("/users", function (req, res) {
+  console.log("Email: " + req.body.email)
+  console.log("Pass: " + req.body.password)
+
+  res.send("Datos recibidos")
 })
 
 app.listen(8080, function () {
