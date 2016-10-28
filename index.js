68c934b783365d131c5b1d535e82763f97fe1677
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-10-27 21:21:57 -0300

    se agrega establece conexion con mongodb - se crea schema de Usuario - se guardan datos de usuario logeado

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require("express");
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+var Schema = mongoose.Schema;
 var app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/pictures")
+
+var user_schema = new Schema({
+  email: String,
+  password: String
+})
+
+var User = mongoose.model("User", user_schema);
 
 app.use("/app",express.static("public"))
 app.use(bodyParser.json()) // para peticiones application/json
@@ -14,14 +26,22 @@ app.get("/", function (req, res) {
 })
 
 app.get("/login", function (req, res) {
-  res.render("login")
+  User.find(function (err, doc){
+    console.log(doc)
+    res.render("login")
+  })
+
+
 })
 
 app.post("/users", function (req, res) {
-  console.log("Email: " + req.body.email)
-  console.log("Pass: " + req.body.password)
+  
+  var user = new User({email: req.body.email, password: req.body.password});
 
-  res.send("Datos recibidos")
+  user.save(function () {
+    res.send("Datos recibidos")
+  })
+
 })
 
 app.listen(8080, function () {
