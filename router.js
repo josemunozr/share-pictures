8b8db90413b1ee0fafc3e1625bd2de807b3761d9
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-11-06 00:55:46 -0300

    se crean rutas dinamicas CRUD para manejo de imagenes

--- router.js
+++ router.js
@@ -6,4 +6,31 @@ router.get("/", function (req, res) {
   res.render("app/home");
 })
 
+router.get("/pictures/new", function (req, res) {
+  res.render("app/pictures/new")
+});
+
+router.get("/pictures/:id/edit", function (req, res) {
+
+});
+
+router.route("pictures/:id")
+  .get(function (req, res) {
+
+  })
+  .put(function (req, res) {
+
+  })
+  .delete(function (req, res) {
+
+  });
+
+router.route("pictures")
+  .get(function (req, res) {
+
+  })
+  .post(function (req, res) {
+
+  });
+
 module.exports = router;
